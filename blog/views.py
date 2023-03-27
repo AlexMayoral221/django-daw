7956c3c2916487bff712d1618114a70cd2b060c6
Alex Mayoral <reyalex58@gmail.com>
2023-03-26 19:14:22 -0700

    django-daw

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
 
 def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+    post = get_object_or_404(post, pk=pk)
+return render(request, 'blog/templates/blog/post_detail.html', {'post': post})
